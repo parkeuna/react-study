b9706087bc8cd633aaafb370fbbd798ff2d0f5d3
LAPTOP-I0H4MLOF\은아 <b__319@naver.com>
2021-12-01 13:28:50 +0900

    리액트 8일차

--- template/src/App.jsx
+++ template/src/App.jsx
@@ -6,35 +6,35 @@ const App = () => {
   const refInput2 = useRef()
   const fnAdd = () => {
     let name = refInput1.current.value
+    let id = Date.now()
     let obj = {
-      id: Date.now(),
-      name
+      name,
+      id
     }
+    refInput1.current.value = ""
     _setArr([..._arr, obj])
-  }
-  const fnDel = () => {
-    let delName = refInput2.current.value
-    let arr = _arr.filter((v)=>{
-      return delName !== v.id
+  }//fnAdd
+  const fnRemove = () => {
+    let name = refInput2.current.value
+    let arr = _arr.filter((v) => {
+      return v.name !== name
     })
     _setArr(arr)
   }
   return (
     <>
-      <p>이름을 추가하거나 제거하면 목록이 li 객체로 출력됩니다</p>
+      <p>이름을 추가하거나 제거하면 목록이 li객체로 출력됩니다</p>
       <input ref={refInput1} type="text" /> &nbsp;
       <button onClick={fnAdd}>추가</button>
       <br />
       <input ref={refInput2} type="text" /> &nbsp;
-      <button onClick={fnDel}>제거</button>
+      <button onClick={fnRemove} >제거</button>
       <hr />
       <ul>
         {
           _arr.map((v) => {
             return (
-              <li key={v.id}>
-                {v.name}
-              </li>
+              <li key={v.id}>{v.name}</li>
             )
           })
         }
